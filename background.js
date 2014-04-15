1d98b6a76682d66009a0f4af9d3ab34238ef0924
Samantha Schaffer <samanthaeschaffer@gmail.com>
2014-04-16 00:02:11 +0930

    Changed to page action
    Now icon appears on facebook event pages once the invite box is open

--- background.js
+++ background.js
@@ -1,7 +1,22 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'www.facebook.com', urlContains: 'events' },
+            css: ["input[name='checkableitems[]']"]
+          })
+       ],
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
+
+chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, {
                 "file": "contentscript.js"
-            }, function () { // Execute your code
-                console.log("Script Executed .. "); // Notification on Completion
             });
 });
