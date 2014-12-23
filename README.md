e3d5d3ec8be135bb73cfc901eaf20d18fcd422a1
Samantha Schaffer <samanthaeschaffer@gmail.com>
2014-12-23 23:56:10 +1030

    Final commit of Invite All My Friends
    Icon update, read me update, removed not working invite to like page
    function.
    
    FB now has built in select all functionality, making this whole
    application superfluous! This project is now abandoned but the code and
    extension will remain up for historical reference.~

--- README.md
+++ README.md
@@ -3,10 +3,15 @@
 
 -- [**Install the chrome extension**](https://chrome.google.com/webstore/detail/invite-all-my-friends-eas/fjojljlinmigdajkemcmbddjkpeobolk) -- 
 [**Submit feedback and bugs**](https://github.com/samanthaeschaffer/InviteAllMyFriends/issues) --
+
 ***
 Easily invite all your friends to your Facebook events! Click the icon that appears in the omnibox (address bar) when you open the event invitation page to automatically select all your friends.
+***
+*Status:* Abandoned
 
-Version 3.0.0 works with new Facebook update! (November 2014)
+Version 3.0.0 is the final version of this extension.  As of November 2014 this extension does NOT work with Facebook.  I have decided to stop mantaining this extension because Facebook's new update has a built in Select All feature, making this extensions superfluous.
+
+Thank you all for your support.
 
 ***
 
--- src/js/popup.js
+++ src/js/popup.js
@@ -5,48 +5,36 @@ function click(e) {
     chrome.tabs.executeScript(null, {file: "unselect_all.js"});
   } else if (e.target.id === "toggle_select") {
     chrome.tabs.executeScript(null, {file: "toggle_select.js"});
-  } else if (e.target.id === "invite_all") {
-    chrome.tabs.executeScript(null, {file: "invite_all.js"});
   }
 
   window.close();
 }
 
 chrome.tabs.getSelected(null, function(tab) {
-    if (tab.url.indexOf("event") > -1) {
-        // event invite
-        // select all button
-        var div = document.createElement('div');
-        div.id = 'select_all';
-        div.className += 'menu-item';
-        document.body.appendChild(div);
-        var textnode=document.createTextNode("Select All");
-        div.appendChild(textnode);
-        div.addEventListener('click', click);
-        // unselect all button
-        div = document.createElement('div');
-        div.id = 'unselect_all';
-        div.className += 'menu-item';
-        document.body.appendChild(div);
-        textnode=document.createTextNode("Unselect All");
-        div.appendChild(textnode);
-        div.addEventListener('click', click);
-        // toggle selection button
-        div = document.createElement('div');
-        div.id = 'toggle_select';
-        div.className += 'menu-item';
-        document.body.appendChild(div);
-        textnode=document.createTextNode("Toggle Selection");
-        div.appendChild(textnode);
-        div.addEventListener('click', click);
-    } else {
-        // page invite
-        var div = document.createElement('div');
-        div.id = 'invite_all';
-        div.className = 'menu-item';
-        document.body.appendChild(div);
-        var textnode=document.createTextNode("Invite All");
-        div.appendChild(textnode);
-        div.addEventListener('click', click);
-    }
+  // select all button
+  var div = document.createElement('div');
+  div.id = 'select_all';
+  div.className += 'menu-item';
+  document.body.appendChild(div);
+  var textnode=document.createTextNode("Select All");
+  div.appendChild(textnode);
+  div.addEventListener('click', click);
+
+  // unselect all button
+  div = document.createElement('div');
+  div.id = 'unselect_all';
+  div.className += 'menu-item';
+  document.body.appendChild(div);
+  textnode=document.createTextNode("Unselect All");
+  div.appendChild(textnode);
+  div.addEventListener('click', click);
+
+  // toggle selection button
+  div = document.createElement('div');
+  div.id = 'toggle_select';
+  div.className += 'menu-item';
+  document.body.appendChild(div);
+  textnode=document.createTextNode("Toggle Selection");
+  div.appendChild(textnode);
+  div.addEventListener('click', click);
 });
